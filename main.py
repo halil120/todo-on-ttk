e3c60fd63f60dbd7cc8a62d4e27f01679589cbb9
halil120 <haliltual@gmail.com>
2026-09-01 16:07:42 +0300

    можно удалять из листа и добвлять

--- main.py
+++ main.py
@@ -37,25 +37,45 @@ def save_in_json(text):
 
         with open("todo.json", "w", encoding="utf-8") as file:
             json.dump(todo_loaded, file, ensure_ascii=False, indent=2)
+            
+def delete_from_json(text):
+    todo_path = Path("todo.json")
+    if todo_path.exists():
+        with open("todo.json", "r", encoding="utf-8") as file:
+            todo_loaded = json.load(file)
+            
+        todo_loaded.pop(text,None)
 
+        with open("todo.json", "w", encoding="utf-8") as file:
+            json.dump(todo_loaded, file, ensure_ascii=False, indent=2)
+            
+            
+def delete_task():
+    selection = todo_listbox.curselection()
+    index=selection[0]
+    key=todo_listbox.get(index)
+    delete_from_json(key)
+    todo_list_var.set(read_todo())
+
+def add_task():
+    text = entry.get()
+    if text:
+        save_in_json(text)
+    entry.delete(0, END)
+    todo_list_var.set(read_todo())
 
 root = Tk()
 root.title("Список задач")
 root.geometry("400x250+1000+500")
 
 entry = ttk.Entry(root)
-entry.pack(padx=8, pady=8)
+entry.pack(anchor="nw", padx=3, pady=3)
 
+buttons_frame = Frame(root)
+buttons_frame.pack(anchor="nw", padx=3, pady=3)
 
-
-def add_task():
-    text = entry.get()
-    save_in_json(text)
-    entry.delete(0, END)
-    todo_list_var.set(read_todo())
-
-Button(root, text="Добавить", command=add_task).pack()
-
+Button(buttons_frame, text="Добавить", command=add_task).pack(side=LEFT)
+Button(buttons_frame, text="Удалить", command=delete_task).pack(side=LEFT, padx=8, pady=3)
 
 
 todo_list = read_todo()
@@ -63,7 +83,7 @@ todo_list_var = Variable(value=todo_list)
 
 todo_listbox = Listbox(listvariable=todo_list_var)
 
-todo_listbox.pack(anchor=NW, padx=90, pady=5)
+todo_listbox.pack(anchor=NW, padx=3, pady=5)
 
 
 root.mainloop()
